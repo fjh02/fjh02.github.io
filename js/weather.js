399fd4e9f3aafcc2d36bac2d8874f2b3b4b7c809
fjh <719021034@qq.com>
2023-01-09 20:14:47 +0800

    Site updated: 2023-01-09 20:14:46

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "00FFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "3eb34118d7484a158829e05e77932d0e"
+  }
+}
